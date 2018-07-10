a448b1a31246cb427445682f401c3ebc9e3031e4
Josef Benninghoff <jsbenning4@gmail.com>
2018-07-10 00:14:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ class Driver{
   constructor(name, myDate){
     this.name = name;
     this.date = myDate;
-    this.startDate = new Date(this.date);
+    startDate = new Date(this.date);
   }
   
   yearsExperienceFromBeginningOf(year){
