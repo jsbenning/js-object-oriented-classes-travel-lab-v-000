ba16c303c86a88fab57cd27814e95b4d90d4437f
Josef Benninghoff <jsbenning4@gmail.com>
2018-07-10 00:49:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ class Route{
   }
   
   blocksTravelled(){
-    let eastWest = 
+    let eastWest = ["1st Avenue", "2nd Avenue", "3rd Avenue", "Lexington Avenue", "Park", "Madison Avenue", "5th Avenue"];
+    let 
     
     
     
