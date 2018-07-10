4678ffee3c7c8f3288072b2fa12827c419d75b02
Josef Benninghoff <jsbenning4@gmail.com>
2018-07-10 01:01:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ class Route{
     let eastWest = ["1st Avenue", "2nd Avenue", "3rd Avenue", "Lexington Avenue", "Park", "Madison Avenue", "5th Avenue"];
    let myHor = Math.abs((eastWest.indexOf(this.beg.vertical)) - (eastWest.indexOf(this.end.vertical)));
    return myHor + 3;
-   //return Math.abs(myHor - Math.abs(this.beg.horizontal - this.end.horizontal));
+   let myVert =  Math.abs(this.beg.horizontal - this.end.horizontal);
     
   }
   estimatedTime(){
