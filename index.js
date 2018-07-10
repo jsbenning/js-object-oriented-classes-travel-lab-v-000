b1eb1a294f285bd260e9035ce40076181e3f89df
Josef Benninghoff <jsbenning4@gmail.com>
2018-07-10 00:14:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ class Driver{
   constructor(name, myDate){
     this.name = name;
     this.date = myDate;
-    startDate = new Date(this.date);
+    this.startDate = new Date(this.date);
   }
   
 
